1f972874d521725288a71d5a8a6a7430adc2d545
Amrita Pandey <amritap7238@gmail.com>
2025-09-25 09:59:25 +0530

    push

--- model.py
+++ model.py
@@ -40,9 +40,7 @@ input_data_as_numpy_array=np.asarray(input_data)
 input_data_reshaped=input_data_as_numpy_array.reshape(1,-1)
 # standarize the input data
 std_data=scalar.transform(input_data_reshaped)
-print(std_data)
 prediction=classifier.predict(std_data)
-print(prediction)
 if(prediction[0]==0):
     print("The person is not diabetic")
 else:
